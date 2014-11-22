8528382fed504ba4c8dd3899d6f95515b19590a1
Klas Eskilson <klas.eskilson@gmail.com>
2014-11-22 15:56:15 +0100

    Add yeoman angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mayThisNightNeverEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mayThisNightNeverEndApp
+ */
+angular.module('mayThisNightNeverEndApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
