963192e32632c01c2e80de9c4b87a10f41d6550a
Said Özcan <saidozcn@gmail.com>
2014-11-22 21:10:13 +0100

    Trying to send request

---
+++ app/config/other.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'access_token' => 'EJS5WDQUMVK1KEFYLYBKX0FLD3FTQB1V41BHH2C14PKAXXUZ'
+);
--- app/controllers/VenueController.php
+++ app/controllers/VenueController.php
@@ -1,4 +1,5 @@
 <?php
+Requests::register_autoloader();
 
 class VenueController extends BaseController {
 
@@ -14,17 +15,40 @@ class VenueController extends BaseController {
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
 	*/
+	$oauthToken = Config::get('other.access_token');
+	$url = 'https://api.foursquare.com/v2/venues/search?ll={$latitude},{$longitude}&oauth_token='.$oauthToken.'&categoryId={$categoryId}&v=20141122';
+	
 
+	$venueCategories = array(
+		'rock' => '4bf58dd8d48988d1e9931735',
+		'club' => '52e81612bcbc57f1066b7a35', //currently: Club House, nightlife => 4d4b7105d754a06376d81259
+		'dinner' => '4d4b7105d754a06374d81259',
+		'jazz' => '4bf58dd8d48988d1e7931735',
+		'piano' => '4bf58dd8d48988d1e8931735'
+	);
 	public function getVenues()
 	{
-		
-		if(Input::has('latitude') && Input::has('longitude') && Input::has('interests'))
+		$response = array('status' => 'failed', 'message' => '');
+		if (!Input::has('latitude')) 
 		{
-			
+			$response['message'] = 'Latitude is required.';
+		}
+		if(!Input::has('longitude'))
+		{
+			$response['message'] = 'Longitude is required';
+		}
+		else if(!Input::has('interest'))
+		{
+			$response['interest'] = 'One interest is required';
 		}
 		else
 		{
-
+			$url = str_replace("{$latitude}", Input::get('latitude'), $url);
+			$url = str_replace("{$longitude}", Input::get('longitude'), $url);
+			$url = str_replace("{$categoryId}", $venueCategories[Input::get('interest')], $url);
+			$response = Requests::get('https://github.com/timeline.json');
+			var_dump($response->body);
 		}
+		return Response::json($response);
 	}
 }
